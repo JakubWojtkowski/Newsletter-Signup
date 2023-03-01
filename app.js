d196ba0a8a9b7cd85313c917aca6b8d7eed28872
wojtko <j.w2001@onet.pl>
2023-03-01 16:38:07 +0100

    Add routing and css changes

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static());
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
