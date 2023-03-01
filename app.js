caea3463ca19ca80a90deadf9d507415ac74ab18
wojtko <j.w2001@onet.pl>
2023-03-01 19:08:19 +0100

    Make posting data to Mailchimp's servers via API works

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
-app.use(express.static());
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -16,6 +17,57 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
+app.post('/', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us13.api.mailchimp.com/3.0/lists/6e37105dc2";
+
+    const options = {
+        method: "POST",
+        auth: "jakub1:7906f1f24c312de406fbd0fa9dae2e66-us13"
+    };
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
 });
+
+
+
+// API key
+// 7906f1f24c312de406fbd0fa9dae2e66-us13
+
+//  audience/list ID
+// 6e37105dc2
